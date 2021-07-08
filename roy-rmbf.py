31c42e6fdfd8ab26c42999add8e4ad8a1c972210
royarasoffia <85897883+royarasoffia@users.noreply.github.com>
2021-07-09 02:23:03 +0700

    Update roy-rmbf.py

--- roy-rmbf.py
+++ roy-rmbf.py
@@ -676,7 +676,7 @@ class __crack__:
             pw = pw.lower()
             try: os.mkdir('results')
             except: pass
-		user_agent1 = random.choice(['Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14',
+	    user_agent1 = random.choice(['Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0) Opera 12.14',
 	'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:26.0) Gecko/20100101 Firefox/26.0',
 	'Mozilla/5.0 (X11; U; Linux x86_64; en-US; rv:1.9.1.3) Gecko/20090913 Firefox/3.5.3',
 	'Mozilla/5.0 (Windows; U; Windows NT 6.1; en; rv:1.9.1.3) Gecko/20090824 Firefox/3.5.3 (.NET CLR 3.5.30729)',
